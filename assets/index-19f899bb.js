445e4383096f6f548ccec6d13d64973b6d6d9a4f
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2026-01-19 14:33:39 -0300

    Updates

--- assets/index-19f899bb.js
+++ assets/index-19f899bb.js
@@ -8130,7 +8130,7 @@ function Contact() {
   );
 }
 const getImagePath = (path) => {
-  const basePath = "/";
+  const basePath = "/leo_portifolio/";
   const cleanPath = path.startsWith("/") ? path.slice(1) : path;
   return `${basePath}${cleanPath}`;
 };
