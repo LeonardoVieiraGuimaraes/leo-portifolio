e6a863a4612e72449e3f53e809f897352c7eb97d
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2026-01-19 13:59:29 -0300

    feat: substituir botões por currículos - adicionar currículo Google Docs e remover Ver projetos e Fale comigo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,12 +42,14 @@ export default function Hero() {
           </p>
 
           <div className="flex flex-wrap gap-3">
-            <a href="#projectsDeveloper" className="button flex items-center gap-2">
-              <HiArrowTopRightOnSquare className="h-5 w-5" />
-              Ver projetos
-            </a>
-            <a href="#contact" className="button-secondary flex items-center gap-2">
-              Fale comigo
+            <a
+              href="https://docs.google.com/document/d/1hLeOBX7zAz-BtYLxXKWK6RtYEWqWnCo4Z5IA5Kvwzsk/edit?usp=drive_link"
+              target="_blank"
+              rel="noopener"
+              className="button flex items-center gap-2"
+            >
+              <HiArrowDownTray className="h-5 w-5" />
+              Currículo
             </a>
             <a
               href="https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4869231A0&tokenCaptchar=03AFcWeA5Xz3lD3vn5HMS9GhkTKnLTFNnn8jIdHmV_gGRxrMmhMj41i_6Lu2oqfAtLP5wyFXdVZJpjXvvBdtYwHXjTs7bE8nxmxjvutCykBoUD2ABe3QL8PkNAaHKTtLibwnOmDCBThU6EIF0OCwoUk3e6_hV4UtOLpidaeAc6fBZ14zZG19qOnFiRwAKpKlyL0jk1LNgurGjYKluX58WYGzMccWrP14UUtkFZSYCZDIBW4iSf0xGtUqMQwkZc_ESopuXY1_gWxABMsK8llBEOZIvkifBmUADeP1rEEQbejucQ03IqK8ZGZfN99DsnBlHuL9GVfkhai8JypHpWhybU2GuFky2xySYJMSxkDwiepFsyOuYY07WsmkoTjvMSawClnvm06zTG6_6WgBXjejxcFD3D3Vfa-Ay7FpMvh3pMXn5k0bNz2U374mFAC7Iv4HXSjcDgNAulCcPKNOtn2P_E8qru4Ryhl1nhWmctHNrd0mMifRWb9T1DzKINX51NK7NmU6ZOFE-LO4MpwNgG6xl-ncQYWWolaYe7BTRm1rPxdHJWH_GeLHQnRFeaOLWrtvZMO6tbdUw1hmo4"
