df1206eb0f612b424244a042c7e6004eb8af1f65
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2026-01-29 12:07:50 -0300

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // Configuração padrão e simples para Vite + React
 export default defineConfig({
   plugins: [react()],
+  base: "/leo-portifolio/",
   build: {
     outDir: "dist"
   }
