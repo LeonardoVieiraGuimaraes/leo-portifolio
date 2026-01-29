2550a4db50f8cd2a52e5168963fcfd344a6c4cdf
LeonardoVieiraGuimaraes <LeonardoVieiraGuimaraes@users.noreply.github.com>
2026-01-29 14:45:56 +0000

    deploy: 9c76c7f9797e2e2237fe1fc9bf825c5552610f1c

--- assets/index-f4f92f0c.js
+++ assets/index-f4f92f0c.js
@@ -7586,7 +7586,7 @@ var m = reactDomExports;
 }
 const scriptRel = "modulepreload";
 const assetsURL = function(dep) {
-  return "/leo-portifolio/" + dep;
+  return "/" + dep;
 };
 const seen = {};
 const __vitePreload = function preload(baseModule, deps, importerUrl) {
@@ -10349,7 +10349,7 @@ function Contact() {
   );
 }
 const getImagePath = (path) => {
-  const basePath = "/leo-portifolio/";
+  const basePath = "/";
   const cleanPath = path.startsWith("/") ? path.slice(1) : path;
   return `${basePath}${cleanPath}`;
 };
